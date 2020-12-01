98a4d220cf424b395b328349ec482581925a018d
tomtom <tom842257@gmail.com>
2020-12-01 14:14:50 +0000

    column status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_112534) do
+ActiveRecord::Schema.define(version: 2020_12_01_141238) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
